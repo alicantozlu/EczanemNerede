122376d60e807da854a7f0284ce3405102add874
Ali Can Tozlu <alicantozlu@gmail.com>
2022-01-03 20:31:04 +0300

    -

--- src/Screens/ContactsScreen.js
+++ src/Screens/ContactsScreen.js
@@ -1,25 +1,51 @@
-import React, {Component} from 'react';
+import {firebase} from '@react-native-firebase/auth';
+import React, {Component, useEffect} from 'react';
 import {View, StyleSheet, SafeAreaView} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import Icon2 from 'react-native-vector-icons/Ionicons';
 import Contact from '../Components/Contact';
 
 const ConstactsScreen = ({navigation}) => {
+   useEffect(() => {}, []);
+
    return (
       <SafeAreaView style={styles.container}>
-         <View style={styles.containerView}>
-            <Contact
-               name="Ali Can Tozlu"
-               subtitle="Hi, I am waiting you over dinner!"
-               onPress={() => {
-                  navigation.navigate('Chat');
-               }}
-            />
-            <View style={styles.seperator} />
-         </View>
+         <ScrollView style={{width: '95%'}}>
+            <View style={styles.containerView}>
+               {chats.map((chat, index) => (
+                  <React.Fragment key={index}>
+                     <Contact
+                        name={chat.users.find(
+                           x => x !== firebase.auth().currentUser.email,
+                        )}
+                        subtitle="No messages yet!"
+                        onPress={() => {
+                           navigation.navigate('Chat');
+                        }}
+                     />
+                     <View style={styles.seperator} />
+                  </React.Fragment>
+               ))}
+            </View>
+         </ScrollView>
       </SafeAreaView>
    );
 };
+
+const chats = [
+   {
+      users: ['r@a.com', 'b@b.com'],
+      messages: [],
+   },
+   {
+      users: ['a@a.com', 'b@b.com'],
+      messages: [],
+   },
+   {
+      users: ['a@a.com', 'b@b.com'],
+      messages: [],
+   },
+];
 
 const styles = StyleSheet.create({
    container: {
@@ -29,8 +55,8 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
    },
    containerView: {
-      width: '95%',
-      height: '93%',
+      width: '100%',
+      height: '100%',
       flexDirection: 'column',
       justifyContent: 'flex-start',
       alignItems: 'flex-start',
